7678a0d83e932eace1820f1c0ac239445f21dc41
bibhasroy93 <butunroy@gmail.com>
2021-10-19 04:42:12 +0530

    commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -55,14 +55,14 @@ const Image = styled.img`
 
 const InfoContainer = styled.div`
   flex: 1;
-  padding: 50px;
+  padding: 40px;
 `;
 
 const Title = styled.h1`
   font-size: 80px;
 `;
 const Desc = styled.p`
-  margin: 50px 0px;
+  margin: 20px 0px;
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const sliderItems=[
     {
         id:1,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
+        img:"https://i.ibb.co/2hdkjpw/slider2.jpg",
         title:"SUMMER SALE",
         desc:"DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg:"f5fafd",
     },
     {
         id:2,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
+        img:"https://i.ibb.co/tKwCWrb/slider3.jpg",
         title:"WINTER SALE",
         desc:"DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg:"fcf1ed",
     },
     {
         id:3,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
+        img:"https://i.ibb.co/prtKnkv/slider1.jpg",
         title:"PUJA SALE",
         desc:"DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg:"f5fafd",
