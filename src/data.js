097480fb11f088bc40a7d5f6f5f73c717727598e
bibhasroy93 <butunroy@gmail.com>
2021-10-20 05:13:36 +0530

    newCommit

--- src/data.js
+++ src/data.js
@@ -19,5 +19,50 @@ export const sliderItems=[
         title:"PUJA SALE",
         desc:"DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg:"f5fafd",
-    }
-]
+    },
+];
+
+export const categories=[
+    {
+        id:1,
+        img:"https://i.ibb.co/qDMrHKF/tshirt.jpg",
+        title:"SHIRT STYLE!",
+    },
+    {
+        id:2,
+        img:"https://i.ibb.co/tMrhTzV/tshirt1.jpg",
+        title:"LOUNGEWEAR LOVE",
+    },
+    {
+        id:3,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+        title:"LIGHT JACKETS",
+    },  
+];
+
+export const popularProducts=[
+    {
+        id:1,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+    },
+    {
+        id:2,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+    },
+    {
+        id:3,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+    },
+    {
+        id:4,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+    },
+    {
+        id:5,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+    },
+    {
+        id:6,
+        img:"https://i.ibb.co/Br09NFP/jacket.jpg",
+    },
+];
